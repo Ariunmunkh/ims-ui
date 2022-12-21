3269a56ddbf7c07c2319bea6d636693fe8aaf3a0
nyamsambuu <nyamsambuu.d@gmail.com>
2022-12-21 15:07:19 +0800

    design nemsen

--- src/components/pages/shared/MasterPage.js
+++ src/components/pages/shared/MasterPage.js
@@ -1,179 +1,211 @@
-﻿import React, { useState } from 'react'
-import { Routes, Route, useNavigate } from 'react-router-dom'
-import './MasterPage.css';
+﻿import React, { useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import "./MasterPage.css";
 import {
-    MenuFoldOutlined,
-    MenuUnfoldOutlined,
-    UsergroupAddOutlined,
-    SolutionOutlined,
-    FileSearchOutlined,
-    AppstoreAddOutlined,
-    CrownOutlined,
-    UserOutlined,
-    HomeOutlined,
-    LogoutOutlined
-} from '@ant-design/icons';
-import { Layout, Menu, theme } from 'antd';
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UsergroupAddOutlined,
+  SolutionOutlined,
+  FileSearchOutlined,
+  AppstoreAddOutlined,
+  CrownOutlined,
+  UserOutlined,
+  HomeOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu, theme, Space, Button,Typography  } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
 import logo from "../../../assets/images/logo.png";
 
-import UserListPage from '../user/UserListPage'
-import HouseHoldListPage from '../household/HouseHoldListPage'
-import HouseHoldPage from '../household/HouseHoldPage'
-import useToken from '../../system/useToken';
-import useRole from '../../system/useRole';
-import NotFound from './NotFound';
-import AccessDenied from './AccessDenied';
+import UserListPage from "../user/UserListPage";
+import HouseHoldListPage from "../household/HouseHoldListPage";
+import HouseHoldPage from "../household/HouseHoldPage";
+import useToken from "../../system/useToken";
+import useRole from "../../system/useRole";
+import NotFound from "./NotFound";
+import AccessDenied from "./AccessDenied";
 
 const { Header, Sider, Content } = Layout;
-
+const { Text, Link } = Typography;
 export default function MasterPage() {
+  const navigate = useNavigate();
+  const { setToken } = useToken();
+  const { roleid } = useRole();
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
 
-    const navigate = useNavigate();
-    const { setToken } = useToken();
-    const { roleid } = useRole();
-    const [collapsed, setCollapsed] = useState(false);
-    const { token: { colorBgContainer }, } = theme.useToken();
-
-    const getMenu = () => {
-
-        switch (roleid) {
-            case '1':
-                return [
-                    {
-                        key: 'userlist',
-                        icon: <UserOutlined />,
-                        label: 'Хэрэглэгч',
-                    },
-                    {
-                        key: 'householdlist',
-                        icon: <HomeOutlined />,
-                        label: 'Өрхийн бүртгэл',
-                    },
-                    {
-                        icon: <UsergroupAddOutlined />,
-                        label: 'Бүлгийн бүртгэл',
-                    },
-                    {
-                        icon: <SolutionOutlined />,
-                        label: 'Сургалтын бүртгэл',
-                    },
-                    {
-                        icon: <AppstoreAddOutlined />,
-                        label: 'Хөрөнгийн бүртгэл',
-                    },
-                    {
-                        icon: <FileSearchOutlined />,
-                        label: 'Холбон зуучлалын бүртгэл',
-                    },
-                    {
-                        icon: <CrownOutlined />,
-                        label: 'Үр дүн',
-                    },
-                ];
-            case '2':
-                return [
-                    {
-                        key: 'householdlist',
-                        icon: <HomeOutlined />,
-                        label: 'Өрхийн бүртгэл',
-                    },
-                    {
-                        icon: <UsergroupAddOutlined />,
-                        label: 'Бүлгийн бүртгэл',
-                    },
-                    {
-                        icon: <SolutionOutlined />,
-                        label: 'Сургалтын бүртгэл',
-                    },
-                    {
-                        icon: <AppstoreAddOutlined />,
-                        label: 'Хөрөнгийн бүртгэл',
-                    },
-                    {
-                        icon: <FileSearchOutlined />,
-                        label: 'Холбон зуучлалын бүртгэл',
-                    },
-                ];
-            case '3':
-                return [
-                    {
-                        key: 'householdlist',
-                        icon: <HomeOutlined />,
-                        label: 'Өрхийн бүртгэл',
-                    },
-                    {
-                        icon: <UsergroupAddOutlined />,
-                        label: 'Бүлгийн бүртгэл',
-                    },
-                    {
-                        icon: <SolutionOutlined />,
-                        label: 'Сургалтын бүртгэл',
-                    },
-                    {
-                        icon: <AppstoreAddOutlined />,
-                        label: 'Хөрөнгийн бүртгэл',
-                    },
-                ];
-            default:
-                return [];
-        }
+  const getMenu = () => {
+    switch (roleid) {
+      case "1":
+        return [
+          {
+            key: "userlist",
+            icon: <UserOutlined />,
+            label: "Хэрэглэгч",
+          },
+          {
+            key: "householdlist",
+            icon: <HomeOutlined />,
+            label: "Өрхийн бүртгэл",
+          },
+          {
+            icon: <UsergroupAddOutlined />,
+            label: "Бүлгийн бүртгэл",
+          },
+          {
+            icon: <SolutionOutlined />,
+            label: "Сургалтын бүртгэл",
+          },
+          {
+            icon: <AppstoreAddOutlined />,
+            label: "Хөрөнгийн бүртгэл",
+          },
+          {
+            icon: <FileSearchOutlined />,
+            label: "Холбон зуучлалын бүртгэл",
+          },
+          {
+            icon: <CrownOutlined />,
+            label: "Үр дүн",
+          },
+        ];
+      case "2":
+        return [
+          {
+            key: "householdlist",
+            icon: <HomeOutlined />,
+            label: "Өрхийн бүртгэл",
+          },
+          {
+            icon: <UsergroupAddOutlined />,
+            label: "Бүлгийн бүртгэл",
+          },
+          {
+            icon: <SolutionOutlined />,
+            label: "Сургалтын бүртгэл",
+          },
+          {
+            icon: <AppstoreAddOutlined />,
+            label: "Хөрөнгийн бүртгэл",
+          },
+          {
+            icon: <FileSearchOutlined />,
+            label: "Холбон зуучлалын бүртгэл",
+          },
+        ];
+      case "3":
+        return [
+          {
+            key: "householdlist",
+            icon: <HomeOutlined />,
+            label: "Өрхийн бүртгэл",
+          },
+          {
+            icon: <UsergroupAddOutlined />,
+            label: "Бүлгийн бүртгэл",
+          },
+          {
+            icon: <SolutionOutlined />,
+            label: "Сургалтын бүртгэл",
+          },
+          {
+            icon: <AppstoreAddOutlined />,
+            label: "Хөрөнгийн бүртгэл",
+          },
+        ];
+      default:
+        return [];
     }
-    const [menuitem] = useState(getMenu);
+  };
+  const [menuitem] = useState(getMenu);
 
+  const onClick = async (item, key, keyPath, domEvent) => {
+    navigate(item?.key);
+  };
 
-    const onClick = async (item, key, keyPath, domEvent) => {
-        navigate(item?.key);
-    }
+  const onLogoutClick = async (event) => {
+    setToken({ access_token: null });
+    window.location.reload();
+  };
 
-    const onLogoutClick = async (event) => {
+  return (
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <img className="logo" src={logo} alt="logo"></img>
+        <Menu
+          style={{ position: "sticky", top: 0, zIndex: 1 }}
+          theme="dark"
+          mode="inline"
+          onClick={onClick}
+          items={menuitem}
+          className="text-white"
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+            position: "sticky",
+            top: 0,
+            zIndex: 1,
+          }}
+        >
+          {React.createElement(
+            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+            {
+              className: "trigger",
+              onClick: () => setCollapsed(!collapsed),
+            }
+          )}
 
-        setToken({ access_token: null });
-        window.location.reload();
-    }
+          <Space direction="vertical">
+            <Space>
+            <Text code>Нэвтэрсэн хэрэглэгч: <b>ADMIN</b></Text>
+            <Text code>Харьяа дүүрэг: <b>БЗД</b></Text>
+            <Text code>Өрхийн тоо: <b>50</b></Text>
+            </Space>
+          </Space>
 
-    return (
-
-        <Layout>
-            <Sider trigger={null} collapsible collapsed={collapsed}>
-                <img className="logo" src={logo} alt="logo"></img>
-                <Menu
-                    style={{ position: 'sticky', top: 0, zIndex: 1 }}
-                    theme="dark"
-                    mode="inline"
-                    onClick={onClick}
-                    items={menuitem}
-                />
-
-            </Sider>
-            <Layout className="site-layout">
-                <Header style={{ padding: 0, background: colorBgContainer, position: 'sticky', top: 0, zIndex: 1 }} >
-                    {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-                        className: 'trigger',
-                        onClick: () => setCollapsed(!collapsed),
-                    })}
-
-                    <LogoutOutlined onClick={onLogoutClick} />
-
-                </Header>
-                <Content
-                    style={{
-                        margin: '24px 16px',
-                        padding: 24,
-                        minHeight: '90vh',
-                        background: colorBgContainer,
-                    }}
-                >
-                    <Routes>
-
-                        <Route path="/userlist" element={roleid === '1' ? <UserListPage /> : <AccessDenied />} />
-                        <Route path="/householdlist" element={roleid === '1' ? <HouseHoldListPage /> : <AccessDenied />} />
-                        <Route path="/household/:householdid" element={roleid === '1' ? <HouseHoldPage /> : <AccessDenied />} />
-                        <Route path="*" element={<NotFound />} />
-                    </Routes>
-                </Content>
-            </Layout>
-        </Layout>
-
-
-    )
+          <Button
+            type="primary"
+            style={{float:'right', margin:'16px'}}
+            icon={<LogoutOutlined onClick={onLogoutClick} />}
+            size={"middle"}
+          >
+            Гарах
+          </Button>
+        </Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: "90vh",
+            background: colorBgContainer,
+          }}
+        >
+          <Routes>
+            {/* <Route path="/userlist" element={roleid === '1' ? <UserListPage /> : <AccessDenied />} /> */}
+            <Route
+              path="/userlist"
+              element={roleid === "1" ? <UserListPage /> : <AccessDenied />}
+            />
+            <Route
+              path="/householdlist"
+              element={
+                roleid === "1" ? <HouseHoldListPage /> : <AccessDenied />
+              }
+            />
+            <Route
+              path="/household/:householdid"
+              element={roleid === "1" ? <HouseHoldPage /> : <AccessDenied />}
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Content>
+      </Layout>
+    </Layout>
+  );
 }
--- src/components/system/api.js
+++ src/components/system/api.js
@@ -1,49 +1,59 @@
-﻿import axios from 'axios';
-import { message } from 'antd';
-import { getBaseURL } from './apiconfig';
+﻿import axios from "axios";
+import { message } from "antd";
+import { getBaseURL } from "./apiconfig";
 
 let errorMessage = "Амжилтгүй боллоо. ";
 
 const auth = {
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json'
+  "Access-Control-Allow-Origin": "*",
+  "Content-Type": "application/json",
+  'Access-Control-Allow-Credentials': true
 };
 
 const api = axios.create({
-    baseURL: getBaseURL(),
-    withCredentials: false,
-    timeout: 240000,
-    headers: auth
+  baseURL: getBaseURL(),
+  withCredentials: false,
+  timeout: 240000,
+  headers: auth,
+  mode: 'cors'
 });
 
-api.interceptors.request.use(function (config) {
+api.interceptors.request.use(
+  function (config) {
     return config;
-}, function (error) {
+  },
+  function (error) {
     message.error(errorMessage + error.message);
     return Promise.reject(error);
-});
+  }
+);
 
-api.interceptors.response.use(function (response) {
-
+api.interceptors.response.use(
+  function (response) {
     if (response.status !== 200) {
-        message.error(errorMessage + response.message);
+      message.error(errorMessage + response.message);
     }
 
-    if (!response?.config?.ignoreRetType && response.data.rettype !== undefined && response?.data?.rettype !== 0) {
-        message.error(errorMessage + response?.data?.retmsg);
+    if (
+      !response?.config?.ignoreRetType &&
+      response.data.rettype !== undefined &&
+      response?.data?.rettype !== 0
+    ) {
+      message.error(errorMessage + response?.data?.retmsg);
     }
 
     return response;
-}, async function (error) {
+  },
+  async function (error) {
     if (error.response.status === 401) {
-        if (!error.request.responseURL.endsWith("/login")) {
-            message.error(errorMessage + error.response.message);
-        }
-    }
-    else {
-        message.error(errorMessage + error.message);
+      if (!error.request.responseURL.endsWith("/login")) {
+        message.error(errorMessage + error.response.message);
+      }
+    } else {
+      message.error(errorMessage + error.message);
     }
     return Promise.reject(error);
-});
+  }
+);
 
 export { api };
