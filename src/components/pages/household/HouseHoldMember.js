09811693642e9f1455d43edeb409cecf2c46ef03
Ariunmunkh.e <ariunmunkh.e@digitalworks.mn>
2022-12-28 12:18:42 +0800

    update

--- src/components/pages/household/HouseHoldMember.js
+++ src/components/pages/household/HouseHoldMember.js
@@ -1,232 +1,261 @@
 ﻿import React, { useState, useEffect, useCallback } from "react";
 import { useParams } from "react-router-dom";
 import { api } from "../../system/api";
-import { Table, Modal, Form, Button, Input, Switch } from "antd";
+import { Table, Modal, Form, Button, Input, Select, Switch } from "antd";
 import { ExclamationCircleFilled } from "@ant-design/icons";
 import { PlusOutlined } from "@ant-design/icons";
 const { confirm } = Modal;
 export default function HouseHoldMember() {
-  const { householdid } = useParams();
-  const nulldata = {
-    memberid: 0,
-    householdid: householdid,
-    name: null,
-    relationshipid: null,
-    birthdate: null,
-    gender: null,
-    educationlevel: null,
-    employment: null,
-    health: null,
-  };
-
-  const [griddata, setGridData] = useState();
-  const [loading, setLoading] = useState(true);
-  const [formdata] = Form.useForm();
-
-  const fetchData = useCallback(() => {
-    setLoading(true);
-    api
-      .get(
-        `/api/record/households/get_householdmember_list?householdid=${householdid}`
-      )
-      .then((res) => {
-        if (res?.status === 200 && res?.data?.rettype === 0) {
-          setGridData(res?.data?.retdata);
-        }
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, [householdid]);
-
-  const tableOnRow = (record, rowIndex) => {
-    return {
-      onClick: (event) => {
-        getFormData(record.memberid);
-      },
+    const { householdid } = useParams();
+    const nulldata = {
+        memberid: 0,
+        householdid: householdid,
+        name: null,
+        relationshipid: null,
+        birthdate: null,
+        gender: null,
+        educationlevel: null,
+        employment: null,
+        health: null,
     };
-  };
 
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
+    const [griddata, setGridData] = useState();
+    const [relationship, setrelationship] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [formdata] = Form.useForm();
 
-  const gridcolumns = [
-    {
-      title: "Өрхийн гишүүний нэр",
-      dataIndex: "name",
-    },
-    {
-      title: "Өрхийн тэргүүнтэй ямар хамааралтай болох",
-      dataIndex: "relationshipid",
-    },
-    {
-      title: "Төрсөн огноо",
-      dataIndex: "birthdate",
-    },
-    {
-      title: "Хүйс",
-      dataIndex: "gender",
-    },
-    {
-      title: "Боловсролын зэрэг",
-      dataIndex: "educationlevel",
-    },
-    {
-      title: "Хөдөлмөр эрхлэлтийн байдал",
-      dataIndex: "employment",
-    },
-    {
-      title: "Эрүүл мэндийн байдал",
-      dataIndex: "health",
-    },
-  ];
+    const fetchData = useCallback(() => {
+        setLoading(true);
+        api.get(`/api/record/households/get_householdmember_list?householdid=${householdid}`)
+            .then((res) => {
+                if (res?.status === 200 && res?.data?.rettype === 0) {
+                    setGridData(res?.data?.retdata);
+                }
+            })
+            .finally(() => {
+                setLoading(false);
+            });
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
+        api.get(`/api/record/base/get_relationship_list`)
+            .then((res) => {
+                if (res?.status === 200 && res?.data?.rettype === 0) {
+                    setrelationship(res?.data?.retdata);
+                }
+            })
+            .finally(() => {
+                setLoading(false);
+            });
 
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
+    }, [householdid]);
 
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
+    const tableOnRow = (record, rowIndex) => {
+        return {
+            onClick: (event) => {
+                getFormData(record.memberid);
+            },
+        };
+    };
 
-  const showDeleteConfirm = () => {
-    confirm({
-      title: "Устгах уу?",
-      icon: <ExclamationCircleFilled />,
-      //content: 'Some descriptions',
-      okText: "Тийм",
-      okType: "danger",
-      cancelText: "Үгүй",
-      onOk() {
-        onDelete();
-      },
-      onCancel() {
-        //console.log('Cancel');
-      },
-    });
-  };
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
 
-  const onDelete = async () => {
-    await api
-      .delete(
-        `/api/households/delete_householdmember?id=${formdata.getFieldValue(
-          "memberid"
-        )}`
-      )
-      .then((res) => {
-        if (res?.status === 200 && res?.data?.rettype === 0) {
-          setIsModalOpen(false);
-          fetchData();
-        }
-      });
-  };
+    const gridcolumns = [
+        {
+            title: "Өрхийн гишүүний нэр",
+            dataIndex: "name",
+        },
+        {
+            title: "Өрхийн тэргүүнтэй ямар хамааралтай болох",
+            dataIndex: "relationshipname",
+        },
+        {
+            title: "Төрсөн огноо",
+            dataIndex: "birthdate",
+        },
+        {
+            title: "Хүйс",
+            dataIndex: "gender",
+        },
+        {
+            title: "Боловсролын зэрэг",
+            dataIndex: "educationlevel",
+        },
+        {
+            title: "Хөдөлмөр эрхлэлтийн байдал",
+            dataIndex: "employment",
+        },
+        {
+            title: "Эрүүл мэндийн байдал",
+            dataIndex: "health",
+        },
+    ];
 
-  const onFinish = async (values) => {
-    await api
-      .post(`/api/households/set_householdmember`, formdata.getFieldsValue())
-      .then((res) => {
-        if (res?.status === 200 && res?.data?.rettype === 0) {
-          setIsModalOpen(false);
-          fetchData();
-        }
-      });
-  };
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const getFormData = async (memberid) => {
-    await api
-      .get(`/api/households/get_householdmember?id=${memberid}`)
-      .then((res) => {
-        if (res?.status === 200 && res?.data?.rettype === 0) {
-          formdata.setFieldsValue(res?.data?.retdata[0]);
-          showModal();
-        }
-      });
-  };
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
 
-  const newFormData = async () => {
-    formdata.setFieldsValue(nulldata);
-    showModal();
-  };
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
 
-  return (
-    <div>
-      <Button
-        style={{ marginBottom: 16 }}
-        type="primary"
-        icon={<PlusOutlined />}
-        onClick={(e) => newFormData()}
-      >
-        Өрхийн гишүүн нэмэх
-      </Button>
+    const showDeleteConfirm = () => {
+        confirm({
+            title: "Устгах уу?",
+            icon: <ExclamationCircleFilled />,
+            //content: 'Some descriptions',
+            okText: "Тийм",
+            okType: "danger",
+            cancelText: "Үгүй",
+            onOk() {
+                onDelete();
+            },
+            onCancel() {
+                //console.log('Cancel');
+            },
+        });
+    };
 
-      <Table
-        loading={loading}
-        columns={gridcolumns}
-        dataSource={griddata}
-        onRow={tableOnRow}
-        pagination={false}
-        rowKey={(record) => record.memberid}
-      ></Table>
-      <Modal
-        forceRender
-        title="Өрхийн гишүүн"
-        open={isModalOpen}
-        onOk={onFinish}
-        onCancel={handleCancel}
-        centered
-        bodyStyle={{ height: "70vh" }}
-        style={{ float: "right", paddingTop: "24px", paddingRight: "10px" }}
-        footer={[
-          <Button
-            key="delete"
-            danger
-            onClick={showDeleteConfirm}
-            hidden={formdata.getFieldValue("memberid") === 0}
-          >
-            Устгах
-          </Button>,
-          <Button key="cancel" onClick={handleCancel}>
-            Болих
-          </Button>,
-          <Button key="save" type="primary" onClick={onFinish}>
-            Хадгалах
-          </Button>,
-        ]}
-      >
-        <Form form={formdata} labelCol={{ span: 8 }} wrapperCol={{ span: 14 }}>
-          <Form.Item name="memberid" label="Дугаар" hidden={true}>
-            <Input />
-          </Form.Item>
-          <Form.Item name="householdid" label="Өрхийн дугаар" hidden={true}>
-            <Input />
-          </Form.Item>
-          <Form.Item name="name" label="Өрхийн гишүүний нэр">
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="relative"
-            label="Өрхийн тэргүүнтэй ямар хамааралтай болох"
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item name="birthdate" label="Төрсөн огноо">
-            <Input />
-          </Form.Item>
-          <Form.Item name="gender" label="Хүйс">
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="istogether"
-            label="Одоо тантай хамт амьдарч байгаа юу ?"
-            valuePropName="checked"
-          >
-            <Switch checkedChildren="Тийм" unCheckedChildren="Үгүй" />
-          </Form.Item>
-        </Form>
-      </Modal>
-    </div>
-  );
+    const onDelete = async () => {
+        await api.delete(`/api/record/households/delete_householdmember?id=${formdata.getFieldValue("memberid")}`)
+            .then((res) => {
+                if (res?.status === 200 && res?.data?.rettype === 0) {
+                    setIsModalOpen(false);
+                    fetchData();
+                }
+            });
+    };
+
+    const onFinish = async (values) => {
+        await api
+            .post(`/api/record/households/set_householdmember`, formdata.getFieldsValue())
+            .then((res) => {
+                if (res?.status === 200 && res?.data?.rettype === 0) {
+                    setIsModalOpen(false);
+                    fetchData();
+                }
+            });
+    };
+
+    const getFormData = async (memberid) => {
+        await api
+            .get(`/api/record/households/get_householdmember?id=${memberid}`)
+            .then((res) => {
+                if (res?.status === 200 && res?.data?.rettype === 0) {
+                    formdata.setFieldsValue(res?.data?.retdata[0]);
+                    showModal();
+                }
+            });
+    };
+
+    const newFormData = async () => {
+        formdata.setFieldsValue(nulldata);
+        showModal();
+    };
+
+    return (
+        <div>
+            <Button
+                style={{ marginBottom: 16 }}
+                type="primary"
+                icon={<PlusOutlined />}
+                onClick={(e) => newFormData()}
+            >
+                Өрхийн гишүүн нэмэх
+            </Button>
+
+            <Table
+                loading={loading}
+                columns={gridcolumns}
+                dataSource={griddata}
+                onRow={tableOnRow}
+                pagination={false}
+                rowKey={(record) => record.memberid}
+            ></Table>
+            <Modal
+                forceRender
+                title="Өрхийн гишүүн"
+                open={isModalOpen}
+                onOk={onFinish}
+                onCancel={handleCancel}
+                centered
+                bodyStyle={{ height: "70vh" }}
+                style={{ float: "right", paddingTop: "24px", paddingRight: "10px" }}
+                footer={[
+                    <Button
+                        key="delete"
+                        danger
+                        onClick={showDeleteConfirm}
+                        hidden={formdata.getFieldValue("memberid") === 0}
+                    >
+                        Устгах
+                    </Button>,
+                    <Button key="cancel" onClick={handleCancel}>
+                        Болих
+                    </Button>,
+                    <Button key="save" type="primary" onClick={onFinish}>
+                        Хадгалах
+                    </Button>,
+                ]}
+            >
+                <Form form={formdata} labelCol={{ span: 8 }} wrapperCol={{ span: 14 }} labelAlign="left" labelWrap>
+                    <Form.Item name="memberid" label="Дугаар" hidden={true}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item name="householdid" label="Өрхийн дугаар" hidden={true}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item name="name" label="Өрхийн гишүүний нэр">
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name="relationshipid"
+                        label="Өрхийн тэргүүнтэй ямар хамааралтай болох"
+                    >
+                        <Select style={{ width: '100%' }}>
+                            {relationship?.map((t, i) => (<Select.Option key={i} value={t.relationshipid}>{t.name}</Select.Option>))}
+                        </Select>
+                    </Form.Item>
+                    <Form.Item name="birthdate" label="Төрсөн огноо">
+                        <Input />
+                    </Form.Item>
+                    <Form.Item name="gender" label="Хүйс">
+                        <Select
+                            style={{ width: '100%' }}
+                            options={[
+                                {
+                                    value: 0,
+                                    label: 'Эрэгтэй',
+                                },
+                                {
+                                    value: 1,
+                                    label: 'Эмэгтэй',
+                                }
+                            ]}
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        name="istogether"
+                        label="Одоо тантай хамт амьдарч байгаа юу ?"
+                        valuePropName="checked"
+                    >
+                        <Switch checkedChildren="Тийм" unCheckedChildren="Үгүй" />
+                    </Form.Item>
+
+                    <Form.Item name="educationlevel" label="Боловсролын зэрэг">
+                        <Input />
+                    </Form.Item>
+                    <Form.Item name="employment" label="Хөдөлмөр эрхлэлтийн байдал">
+                        <Input />
+                    </Form.Item>
+                    <Form.Item name="health" label="Эрүүл мэндийн байдал">
+                        <Input />
+                    </Form.Item>
+
+                </Form>
+            </Modal>
+        </div>
+    );
 }
