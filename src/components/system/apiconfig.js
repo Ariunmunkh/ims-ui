ee19b09a9239dbe14dd11bd403fd101c0a789479
Ariunmunkh.e <ariunmunkh.e@digitalworks.mn>
2023-02-19 15:16:13 +0800

    update

--- src/components/system/apiconfig.js
+++ src/components/system/apiconfig.js
@@ -1,6 +1,6 @@
 export const getBaseURL = () => {
     let apiBaseUrl = 'http://188.166.242.38:8080';
-    if (window?.location?.hostname === 'localhost') {
+    if (window?.location?.hostname === 'localhost' && false) {
         apiBaseUrl = 'http://localhost:8080/';
     }
     return apiBaseUrl;
