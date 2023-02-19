ab04147fdd199877d9df2aba900dd6feac0232f7
Ariunmunkh.e <ariunmunkh.e@digitalworks.mn>
2023-02-19 15:09:24 +0800

    update

--- src/components/pages/coach/MapPage.js
+++ src/components/pages/coach/MapPage.js
@@ -1,34 +1,99 @@
-import React from "react";
-import { ReactBingmaps } from "react-bingmaps";
-export default function MapPage() {
-  return (
-    <div style={{ width: "100%", height: "100%" }}>
-      <iframe
-        width="100%"
-        height={550}
-        src="https://lookerstudio.google.com/embed/reporting/0e0992ed-12b0-4d23-b60e-6e04ff144841/page/jfQED"
-        frameBorder={0}
-        style={{ border: 0 }}
-        allowFullScreen
-      />
+import React, { useState, useEffect, useCallback } from "react";
+import BingMapsReact from "bingmaps-react";
+import { api } from "../../system/api";
+import useUserInfo from "../../system/useUserInfo";
+import { Spin, Select, Typography, Row, Col } from "antd";
+const { Text } = Typography;
 
-      <iframe
-        src="https://www.google.com/maps/d/embed?mid=1RpECl3oS2pvK_hdq6rP3zXIv1qCubuM&ehbc=2E312F"
-        title="map"
-        width="100%"
-        height="100%"
-      ></iframe>
-      <ReactBingmaps
-        bingmapKey="Al5_dVdiKO78I00m1mkAwVz7EmrK9ylIMTqI7qzQvwo2LSruRN2OkQFeRAsIaI24"
-        supportedMapTypes={["aerial", "canvasDark"]}
-        disableStreetside={true}
-        pushPins={[
-          {
-            location: [47.899168, 106.925506],
-            option: { color: "red" },
-          },
-        ]}
-      ></ReactBingmaps>
-    </div>
-  );
+export default function MapPage() {
+
+    const { userinfo } = useUserInfo();
+    const [loading, setloading] = useState(true);
+    const [pushPins, setdata] = useState([]);
+    const [coachlist, setcoachlist] = useState([]);
+    const [coachid, setcoachid] = useState(0);
+    const [districtlist, setdistrictlist] = useState([]);
+    const [districtid, setdistrictid] = useState(0);
+
+    const fetchData = useCallback(async () => {
+        setloading(true);
+        let tcoachid = userinfo.coachid;
+        tcoachid = tcoachid || tcoachid === '' ? '0' : tcoachid;
+        if (coachid)
+            tcoachid = coachid;
+
+        await api.get(`/api/record/households/get_household_location?coachid=${tcoachid}&districtid=${districtid}`)
+            .then((res) => {
+                if (res?.status === 200 && res?.data?.rettype === 0) {
+                    setdata(res?.data?.retdata);
+                }
+            })
+            .finally(() => {
+                setloading(false);
+            });
+
+    }, [userinfo, districtid, coachid]);
+
+    useEffect(() => {
+        api.get(`/api/record/base/get_district_list`)
+            .then((res) => {
+                if (res?.status === 200 && res?.data?.rettype === 0) {
+                    res?.data?.retdata.push({ districtid: 0, name: 'хоосон' });
+                    setdistrictlist(res?.data?.retdata);
+                }
+            });
+        api.get(`/api/record/coach/get_coach_list`)
+            .then((res) => {
+                if (res?.status === 200 && res?.data?.rettype === 0) {
+                    res?.data?.retdata.push({ coachid: 0, name: 'хоосон' });
+                    setcoachlist(res?.data?.retdata);
+                }
+            });
+        fetchData();
+    }, [fetchData]);
+
+    return (
+
+        <Spin spinning={loading}>
+            <Row gutter={16} style={{ marginTop: 16, marginBottom: 16 }}>
+                <Col>
+                    <Text>Дүүрэг</Text>
+                </Col>
+                <Col>
+                    <Select style={{ width: "150px" }} value={districtid} onChange={(value) => { setcoachid(0); setdistrictid(value); }}>
+                        {districtlist?.map((t, i) => (
+                            <Select.Option key={i} value={t.districtid}>
+                                {t.name}
+                            </Select.Option>
+                        ))}
+                    </Select>
+                </Col>
+                <Col>
+                    <Text>Коуч</Text>
+                </Col>
+                <Col>
+                    <Select style={{ width: "150px" }} value={coachid} onChange={(value) => setcoachid(value)}>
+                        {coachlist?.map((t, i) => (
+                            <Select.Option key={i} value={t.coachid}>
+                                {t.name}
+                            </Select.Option>
+                        ))}
+                    </Select>
+                </Col>
+            </Row >
+            <Row >
+                <Col style={{ width: "80%", height: "80vh" }}>
+                    <div style={{ width: "100%", height: "80vh" }}>
+                        <BingMapsReact
+                            bingMapsKey="Al5_dVdiKO78I00m1mkAwVz7EmrK9ylIMTqI7qzQvwo2LSruRN2OkQFeRAsIaI24"
+                            pushPins={pushPins}
+                        />
+                    </div>
+                </Col>
+                <Col>
+                    <Text>Коуч</Text>
+                </Col>
+            </Row >
+        </Spin >
+    );
 }
--- src/components/system/apiconfig.js
+++ src/components/system/apiconfig.js
@@ -1,4 +1,7 @@
 export const getBaseURL = () => {
     let apiBaseUrl = 'http://188.166.242.38:8080';
+    if (window?.location?.hostname === 'localhost') {
+        apiBaseUrl = 'http://localhost:8080/';
+    }
     return apiBaseUrl;
 }
