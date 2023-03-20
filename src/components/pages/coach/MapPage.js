de4b4bd520dd2acbc0c4f3460e21a02146a30e84
Nyamsambuu <nyamsambuu.d@gmail.com>
2023-03-20 09:59:59 +0800

    Dashboard oruulsan and map oruulsan

--- src/components/pages/coach/MapPage.js
+++ src/components/pages/coach/MapPage.js
@@ -1,158 +1,80 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { api } from "../../system/api";
 import useUserInfo from "../../system/useUserInfo";
-import { SearchOutlined } from '@ant-design/icons';
-import { Spin, Select, Typography, Row, Col, Button } from "antd";
 import _ from "lodash";
-const { Text } = Typography;
 
 export default function MapPage() {
-    const { userinfo } = useUserInfo();
-    const [loading, setloading] = useState(true);
-    const [pushPins, setdata] = useState([]);
-    const [coachlist, setcoachlist] = useState([
-        { coachid: 0, coachname: "хоосон" },
-    ]);
-    const [coachid, setcoachid] = useState(userinfo.coachid * 1);
-    const [districtlist, setdistrictlist] = useState([
-        { districtid: 0, districtname: "хоосон" },
-    ]);
-    const [districtid, setdistrictid] = useState(0);
+  const { userinfo } = useUserInfo();
+  const [loading, setloading] = useState(true);
+  const [pushPins, setdata] = useState([]);
+  const [coachlist, setcoachlist] = useState([
+    { coachid: 0, coachname: "хоосон" },
+  ]);
+  const [coachid, setcoachid] = useState(userinfo.coachid * 1);
+  const [districtlist, setdistrictlist] = useState([
+    { districtid: 0, districtname: "хоосон" },
+  ]);
+  const [districtid, setdistrictid] = useState(0);
 
-    const fetchData = useCallback(async () => {
+  const fetchData = useCallback(async () => {
+    setloading(true);
 
-        setloading(true);
+    await api
+      .get(
+        `/api/record/households/get_household_location?coachid=${coachid}&districtid=${districtid}`
+      )
+      .then((res) => {
+        if (res?.status === 200 && res?.data?.rettype === 0) {
+          setdata(res?.data?.retdata);
 
-        await api
-            .get(
-                `/api/record/households/get_household_location?coachid=${coachid}&districtid=${districtid}`
-            )
-            .then((res) => {
-                if (res?.status === 200 && res?.data?.rettype === 0) {
-                    setdata(res?.data?.retdata);
+          var tcoach = _.uniqBy(res?.data?.retdata, "coachid");
+          tcoach.push({ coachid: 0, coachname: "хоосон" });
+          tcoach.sort((a, b) => a.coachid - b.coachid);
+          tcoach = tcoach.filter((x) => x.coachid != null);
+          setcoachlist(tcoach);
 
-                    var tcoach = _.uniqBy(res?.data?.retdata, "coachid");
-                    tcoach.push({ coachid: 0, coachname: "хоосон" });
-                    tcoach.sort((a, b) => a.coachid - b.coachid);
-                    tcoach = tcoach.filter(x => x.coachid != null);
-                    setcoachlist(tcoach);
+          var tdistrict = _.uniqBy(res?.data?.retdata, "districtid");
+          tdistrict.push({ districtid: 0, districtname: "хоосон" });
+          tdistrict.sort((a, b) => a.districtid - b.districtid);
+          tdistrict = tdistrict.filter((x) => x.districtid != null);
+          setdistrictlist(tdistrict);
+        }
+      })
+      .finally(() => {
+        setloading(false);
+      });
+  }, [coachid, districtid]);
 
-                    var tdistrict = _.uniqBy(res?.data?.retdata, "districtid");
-                    tdistrict.push({ districtid: 0, districtname: "хоосон" });
-                    tdistrict.sort((a, b) => a.districtid - b.districtid);
-                    tdistrict = tdistrict.filter(x => x.districtid != null);
-                    setdistrictlist(tdistrict);
-                }
-            })
-            .finally(() => {
-                setloading(false);
-            });
-    }, [coachid, districtid]);
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
-    useEffect(() => {
-        fetchData();
-    }, [fetchData]);
-
-    return (
-        <div>
-            <Spin spinning={loading}>
-                <Row gutter={16} style={{ marginTop: 16, marginBottom: 16 }}>
-                    <Col>
-                        <Text>Дүүрэг</Text>
-                    </Col>
-                    <Col>
-                        <Select
-                            style={{ width: "150px" }}
-                            value={districtid}
-                            onChange={(value) => {
-                                setcoachid(0);
-                                setdistrictid(value);
-                            }}
-                        >
-                            {districtlist?.map((t, i) => (
-                                <Select.Option key={i} value={t.districtid}>
-                                    {t.districtname}
-                                </Select.Option>
-                            ))}
-                        </Select>
-                    </Col>
-                    <Col>
-                        <Text>Коуч</Text>
-                    </Col>
-                    <Col>
-                        <Select
-                            style={{ width: "150px" }}
-                            value={coachid}
-                            onChange={(value) => setcoachid(value)}
-                        >
-                            {coachlist?.map((t, i) => (
-                                <Select.Option key={i} value={t.coachid}>
-                                    {t.coachname}
-                                </Select.Option>
-                            ))}
-                        </Select>
-                    </Col>
-                    <Col>
-                        <Button type="primary" onClick={() => fetchData()} icon={<SearchOutlined />}>
-                            Хайх
-                        </Button>
-                    </Col>
-                </Row>
-            </Spin>
-
-            <div className="row">
-                <div className="col-md-12">
-                    <div className="card">
-                        <div className="card-header">
-                            <h5 className="card-title">Өрхийн байршлын мэдээлэл</h5>
-                        </div>
-                        <div className="card-body p-0">
-                            <div className="d-md-flex">
-                                <div className="p-1 flex-fill" >
-                                    <div
-                                        id="world-map-markers"
-                                        style={{ height: 700 }}
-                                    >
-
-
-                                    </div>
-                                </div>
-                                <div className="card-pane-right bg-success pt-2 pb-2 pl-4 pr-4 text-white text-center">
-                                    <div className="description-block mb-4">
-                                        <div className="sparkbar pad" data-color="#fff">
-                                            ӨРХИЙН ТОО
-                                        </div>
-                                        <h3 className="description-header">{pushPins.length}</h3>
-                                    </div>
-                                    <div className="description-block mb-4">
-                                        <div className="sparkbar pad" data-color="#fff">
-                                            ХОРООНЫ ТОО
-                                        </div>
-                                        <h3 className="description-header">{`${_.uniqBy(pushPins, "section").length
-                                            }`}</h3>
-                                    </div>
-                                    <div className="description-block mb-4">
-                                        <div className="sparkbar pad" data-color="#fff">
-                                            КОУЧИЙН ТОО
-                                        </div>
-                                        <h3 className="description-header">{`${_.uniqBy(pushPins, "coachid").length
-                                            }`}</h3>
-                                    </div>
-                                    <div className="description-block mb-4">
-                                        <div className="sparkbar pad" data-color="#fff">
-                                            БҮЛГИЙН ТОО
-                                        </div>
-                                        <h3 className="description-header">{`${_.uniqBy(pushPins, "householdgroupid").length
-                                            }`}</h3>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-
-                </div>
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card">
+            <div className="card-header">
+              <h5 className="card-title">Өрхийн байршлын мэдээлэл</h5>
             </div>
+            <div className="card-body p-0">
+              <div className="d-md-flex">
+                <div className="p-1 flex-fill">
+                  <iframe
+                    title="location"
+                    width="100%"
+                    height="800"
+                    src="https://app.powerbi.com/view?r=eyJrIjoiODA4NTBkNTgtZWQ0OS00ZGY1LTkyY2QtZjJmMGI2NTUxYjJhIiwidCI6IjJlNjdjZjFhLTJmYmItNDcxYS04ZjM3LWJlYjViNjg1ZTE5YSIsImMiOjEwfQ%3D%3D"
+                    frameBorder={0}
+                    allowFullScreen="true"
+                  />
+                </div>
+               
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- src/components/pages/shared/Monitor.js
+++ src/components/pages/shared/Monitor.js
@@ -1,51 +1,16 @@
 import React from "react";
-import { Tabs } from "antd";
-import HouseSwitch from "../monitor/HouseHold/HouseSwitch";
-import SocialProtectionSwitch from "../monitor/SocialProtection/SocialProtectionSwitch";
-import LivelihoodSupportSwitch from "../monitor/LivelihoodSupport/LivelihoodSupportSwitch";
-import FinancialParticipationSwitch from "../monitor/FinancialParticipation/FinancialParticipationSwitch";
-import SocialCapacitySwitch from "../monitor/SocialCapacity/SocialCapacitySwitch";
-import CoachSwitch from "../monitor/Coach/CoachSwitch";
 
 export default function Dashboard() {
-    return (
-        <div>
-            <Tabs
-                style={{ height: '80vh' }}
-                defaultActiveKey="1"
-                items={[
-                    {
-                        label: `Өрхийн ерөнхий мэдээлэл`,
-                        key: "1",
-                        children: <HouseSwitch />,
-                    },
-                    {
-                        label: `Амьжиргааг дэмжих`,
-                        key: "2",
-                        children: <LivelihoodSupportSwitch />,
-                    },
-                    {
-                        label: `Нийгмийн хамгаалал`,
-                        key: "3",
-                        children: <SocialProtectionSwitch />,
-                    },
-                    {
-                        label: `Санхүүгийн оролцоо`,
-                        key: "4",
-                        children: <FinancialParticipationSwitch />,
-                    },
-                    {
-                        label: `Нийгмийн чадавхжилт`,
-                        key: "5",
-                        children: <SocialCapacitySwitch />,
-                    },
-                    {
-                        label: `Коучинг ба менторинг`,
-                        key: "6",
-                        children: <CoachSwitch />,
-                    }
-                ]}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <iframe
+        title="dASHBOARD"
+        width="100%"
+        height="800"
+        src="https://app.powerbi.com/view?r=eyJrIjoiMTM1NjcwZmYtOWM3Yy00MTQwLTg5ZGEtYmFmNjdhYjM4ZTE3IiwidCI6IjJlNjdjZjFhLTJmYmItNDcxYS04ZjM3LWJlYjViNjg1ZTE5YSIsImMiOjEwfQ%3D%3D"
+        frameborder="0"
+        allowFullScreen="true"
+      ></iframe>
+    </div>
+  );
 }
